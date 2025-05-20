c83e13b8ef8cfbee9a61ad0728d51ba887f40c2f
aryfacanha <aryfdesa1@gmail.com>
2025-05-20 20:49:46 -0300

    feat: implement beggining of frontend

--- src/main/java/com/ary/loja/product/ProductService.java
+++ src/main/java/com/ary/loja/product/ProductService.java
@@ -3,6 +3,7 @@ package com.ary.loja.product;
 import java.util.List;
 import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Service
 public class ProductService {
 
+    @Autowired
     private ProductRepository productRepository;
 
     public List<Product> getAllProducts() {
