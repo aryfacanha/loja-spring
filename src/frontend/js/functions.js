f466859885e80c6c3d2576f9dc3965f48dff6e6d
aryfacanha <aryfdesa1@gmail.com>
2025-06-30 18:44:29 -0300

    feat: Add navbar

--- src/frontend/js/functions.js
+++ src/frontend/js/functions.js
@@ -1,3 +1,12 @@
+// Load navbar
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("navbar-placeholder").innerHTML = data;
+    });
+});
+
 // Brands
 
 function getBrands(callback) {
